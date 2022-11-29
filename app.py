2d594574335098de0b94e53a02ecef4099b99b1a
Eduardo Mendes <mendesxeduardo@gmail.com>
2022-11-28 23:53:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,50 @@
 import streamlit as st
-from json import loads
-from pandas import read_csv
-
-st.markdown('''
-# Exibidor de arquivos
-
-## Suba um arquivo e vejamos o que acontece :smile::heart:
-''')
+from PIL import Image, ImageFont, ImageDraw
 
 
-arquivo = st.file_uploader(
-    'Suba seu arquivo aqui!',
-    type=['jpg', 'png', 'py', 'wav', 'csv', 'json']
-)
+def text_on_image(image, text, font_size, color):
+    img = Image.open(image)
+    font = ImageFont.truetype('arial.ttf', font_size)
+    draw = ImageDraw.Draw(img)
 
-st.text_input('Email', max_chars=10)
-st.text_input('Senha', type='password')
+    iw, ih = img.size
+    fw, fh = font.getsize(text)
 
-if arquivo:
-    print(arquivo.type)
-    match arquivo.type.split('/'):
-        case 'application', 'json':
-            st.json(loads(arquivo.read()))
-        case 'image', _:
-            st.image(arquivo)
-        case 'text', 'csv':
-            df = read_csv(arquivo).transpose()
-            st.dataframe(df)
-            st.bar_chart(df)
-        case 'text', 'x-python':
-            st.code(arquivo.read().decode())
-        case 'audio', _:
-            st.audio(arquivo)
+    draw.text(
+        ((iw - fw) / 2, (ih - fh) / 2),
+        text,
+        fill=color,
+        font=font
+    )
+
+    img.save('last_image.jpg')
+
+
+image = st.file_uploader('Uma imagem', type=['jpg'])
+text = st.text_input('Sua marca dágua')
+# color = st.selectbox(
+#     'Cor da sua marca', ['black', 'white', 'red', 'green']
+# )
+
+color = st.color_picker('Escolha uma cor')
+
+font_size = st.number_input('Tamanho da fonte', min_value=50)
+
+if image:
+    result = st.button(
+        'Aplicar',
+        type='primary',
+        on_click=text_on_image,
+        args=(image, text, font_size, color)
+    )
+    if result:
+        st.image('last_image.jpg')
+        with open('last_image.jpg', 'rb') as file:
+            st.download_button(
+                'Baixe agora mesmo sua foto com marca',
+                file_name='image_com_marca.jpg',
+                data=file,
+                mime='image/jpg'
+            )
 else:
-    st.error('Ainda não tenho arquivo!')
+    st.warning('Ainda não temos imagem!')
